a6c35f2d23668828daa43821a7819778278cadba
Peter <pbd281@gmail.com>
2019-05-05 22:00:39 -0400

    Simplify deploy_backend

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -5,11 +5,5 @@ set -e
 # Enter the application's directory
 cd /home/ubuntu/neargreen-v2
 
-# Tear down the server
-docker-compose -f docker-compose.prod.yml down
-
-# Rebuild the server
-docker-compose -f docker-compose.prod.yml build
-
-# Restart the server
-docker-compose -f docker-compose.prod.yml up -d
+# Rebuild and restart the server
+sudo docker-compose -f docker-compose.prod.yml up -d --build
